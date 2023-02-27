861621f483bb5dd256d20a82633cb13a48f2d430
Mayllon Diogo <102414925+MayllonD@users.noreply.github.com>
2023-02-27 11:35:54 +0000

    finalizando o projeto

---
+++ scripty.js
@@ -0,0 +1,8 @@
+const burguer = document.querySelector("#burguer");
+const menuWrapper = document.querySelector("#mobile-menu-wrapper");
+
+function mobileMenu() {
+  burguer.classList.toggle("mobile-menu");
+  menuWrapper.classList.toggle("none");
+  console.log("sss");
+}
